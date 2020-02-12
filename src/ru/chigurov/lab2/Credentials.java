613ce2c0553001ed73ff5b1c35d06eee6f3a7b03
Milhail <vip.moneta95@mail.ru>
2020-02-12 22:49:14 +0700

    Lab2 - in the process creation "Credentials"

--- src/ru/chigurov/lab2/Credentials.java
+++ src/ru/chigurov/lab2/Credentials.java
@@ -4,18 +4,18 @@ import java.util.UUID;
 
 public class Credentials {
 
-    UUID idUser = UUID.randomUUID();
-    String firstName  = "Mikhail";
-    String lastName  = "Chigurov";
-    String patronymic  = "Evgen'evich";
-    String email = "vip.moneta95@mail.ru";
+    UUID idUser;
+    String firstName;
+    String lastName;
+    String patronymic;
+    String email;
 
-    public Credentials(){
-       /* idUser = UUID.randomUUID();
-        firstName = "Mikhail";
-        lastName = "Chigurov";
-        patronymic = "Evgen'evich";
-        email = "vip.moneta95@mail.ru";*/
+    public Credentials(UUID idUser, String firstName, String lastName, String patronymic, String email){
+        this.idUser = UUID.randomUUID();
+        this.firstName = "Mikhail";
+        this.lastName = "Chigurov";
+        this.patronymic = "Evgen'evich";
+        this.email = "vip.moneta95@mail.ru";
     }
 
 }
